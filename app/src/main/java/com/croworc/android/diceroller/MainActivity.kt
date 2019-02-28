c22fc1fca27f4ee6cd119d39b7a126efdc6b0b3a
ikrotzky <ikr@croworc.com>
2019-02-28 20:22:08 +0100

    refactor: Use instance field for dice ImageView

--- app/src/main/java/com/croworc/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/croworc/android/diceroller/MainActivity.kt
@@ -9,14 +9,19 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val btnRoll : Button = findViewById(R.id.roll_button)
         btnRoll.text = "Let's roll!"
         btnRoll.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
 
     }
 
@@ -30,7 +35,6 @@ class MainActivity : AppCompatActivity() {
             5    -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }

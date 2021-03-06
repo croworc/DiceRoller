fc351eb98cfb28d46b6e72f858d41983ababba08
ikrotzky <ikr@croworc.com>
2021-03-06 11:34:25 +0100

    [main] doc: Add method comments

--- app/src/main/java/com/croworc/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/croworc/android/diceroller/MainActivity.kt
@@ -25,8 +25,15 @@ class MainActivity : AppCompatActivity() {
 
         diceImage = findViewById(R.id.dice_image)
 
+        // Restore the saved state, if any
         savedInstanceState?.run {
-            val drawableResource: Int = getInt(KEY_DICE_RESOURCE_ID)
+            // If the user has pushed the [ROLL] button already, the image resource ID
+            // of the ImageView's drawable has been saved in the view's `tag` property.
+            // In onSaveInstanceState the ID stored in this tag gets put into the bundle.
+            // Here, we first check whether there's such an ID stored in the bundle, because
+            // if the device was rotated *before* the dice was rolled, then there's no ID
+            // stored in the tag property and thus also no ID in the bundle.
+            val drawableResource = getInt(KEY_DICE_RESOURCE_ID)
             if (drawableResource != 0) {
                 diceImage.setImageResource(drawableResource)
                 diceImage.tag = drawableResource
@@ -36,6 +43,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        // If the dice has been rolled already, then we've put the drawable's resource ID
+        // into the ImageView's `tag` property.
+        // Here, we save this resource ID from the tag property into the bundle.
         if (diceImage.tag != null) {
             outState.putInt(KEY_DICE_RESOURCE_ID, diceImage.tag as Int)
         }
@@ -51,6 +61,8 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
         diceImage.setImageResource(drawableResource)
+        // We'll store away the drawable resource ID into the view's `tag` property,
+        // so that we can save it in onSaveInstanceState, later and restore it in onCreate.
         diceImage.tag = drawableResource
     }
 }
